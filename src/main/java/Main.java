b76ad6c9768b3b93ea2a935cde746ae712dee5aa
ansa1 <abirovansat22@gmail.com>
2018-09-21 23:47:50 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,30 +3,44 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
+
+    // constant string to remove BOM from the file
     public static final String UTF8_BOM = "\uFEFF";
 
+    // removing BOM method
     private static String removeUTF8BOM(String s) {
         if (s.startsWith(UTF8_BOM)) {
             s = s.substring(1);
         }
         return s;
     }
-
     public static void main(String[] args) throws IOException {
-        File file = new File("Program.cs");
+        // ok, let's start
+
+        // use same input and output file names as previous homework
+        File file = new File("in.txt");
+        PrintWriter out = new PrintWriter(new File("out.txt"));
+
+        // read all file
         String text = FileUtils.readFileToString(file);
+        // removing BOM to correct work with it
         text = removeUTF8BOM(text);
 
         LexicalAnalyzer analyzer = new LexicalAnalyzer();
-
+        // while we have lexical atoms:
         while (text != null) {
+
+            // trim tabulation
             text = StringUtils.strip(text, " \t");
             String[] res = analyzer.GetNextLexicalAtom(text);
             text = res[1];
             if (res[1] != null)
-                System.out.print(res[0]);
+                out.print(res[0]);
+                //System.out.print(res[0]);
         }
+        out.close();
     }
 }
